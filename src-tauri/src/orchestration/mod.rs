36798f6c2be98416eb618c84dee7fe1a76e279e8
willem445 <willh445447@hotmail.com>
2026-09-06 10:59:41 -0500

    scratch: round 12: usage_series takes its coverage floor from the FILTERED rows (#2011 red round 12) - DO NOT MERGE

--- src-tauri/src/orchestration/mod.rs
+++ src-tauri/src/orchestration/mod.rs
@@ -44587,7 +44587,7 @@ impl OrchRegistry {
         let text = fs::read_to_string(self.group_dir(group).join(USAGE_SERIES_FILE))
             .unwrap_or_default();
         let (all, skipped) = usageseries::parse_series_lines_counted(&text);
-        let first_ts_ms = all.first().map(|r| r.ts_ms());
+        let first_ts_ms = all.iter().find(|r| r.ts_ms() >= since_ms).map(|r| r.ts_ms());
         let rows: Vec<Value> = all
             .iter()
             .filter(|r| r.ts_ms() >= since_ms)
